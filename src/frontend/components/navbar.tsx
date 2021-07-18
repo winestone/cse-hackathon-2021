fd322316750282665e32a77b552f356bfb41121e
Heidi Chan <z5024971@student.unsw.edu.au>
2021-07-18 11:47:20 +1000

    progress bars

--- src/frontend/components/navbar.tsx
+++ src/frontend/components/navbar.tsx
@@ -25,11 +25,11 @@ export default function NavbarComp({ username, clearUsername }: NavbarCompProps)
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto container-fluid">
-            <Nav.Link as={Link} to="/home" className="nav-item">
-              Home
-            </Nav.Link>
             {!username && (
               <>
+                <Nav.Link as={Link} to="/home" className="nav-item">
+                  Home
+                </Nav.Link>
                 <Nav.Link as={Link} to="/login" className="nav-item">
                   Login
                 </Nav.Link>
--- src/frontend/pages/profilePage.tsx
+++ src/frontend/pages/profilePage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Badge, Container, Row, Col } from "react-bootstrap";
+import { Badge, Container, Row, Col, ProgressBar } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import * as api from "@common/api";
 
@@ -7,6 +7,8 @@ export default function Profile(): React.ReactElement {
   const username = sessionStorage.getItem("username");
   const [achievements, setAchievements] = useState<string[]>([]);
   const [profileNo, setProfileNo] = useState(0);
+  const [favGame, setFaveGame] = useState('');
+  const [progress, setProgress] = useState<number[]>([]);
 
   async function getAchievements() {
     const u = sessionStorage.getItem("username");
@@ -20,6 +22,31 @@ export default function Profile(): React.ReactElement {
       setAchievements(a.slice(0, 3));
       const randomNo = u.length % 3;
       setProfileNo(randomNo);
+      if (randomNo == 1) {
+        setFaveGame('Spelling bee')
+      }
+      else if (randomNo === 2) {
+        setFaveGame('Math Blocks');
+      } 
+      else {
+        setFaveGame('Biology Boggle')
+      }
+
+      // progress bars. 
+      let p = []; 
+      for (let i = 0; i < u.length; i++) {
+        let n = u.charCodeAt(i)%100; 
+        if (n < 25) {
+          n += 30; 
+        }
+        p.push(n); 
+      }
+      if (p.length < 4) {
+        for (let x = p.length; x < 4; x++) {
+          p.push(20)
+        }
+      }
+      setProgress(p);
     }
   }
 
@@ -42,8 +69,23 @@ export default function Profile(): React.ReactElement {
                 </div>
               ))}
             </div>
+            <div className="profile-text">
+              <p>Favourite game: <b>{favGame} </b> </p>
+            </div>
+            
           </Col>
-          <Col md={4} />
+          <Col md={4}> </Col>
+        </Row>
+        <Row>
+          <Col md={3}> </Col>
+            <Col md={6}>
+              <ProgressBar animated striped now={progress[0]} className='progress-1' label={'Maths'}/> 
+              <ProgressBar animated striped now={progress[1]}  className='progress-2' label={'English'}/>
+              <ProgressBar animated striped now={progress[2]}  className='progress-3' label={'Science'}/>
+              <ProgressBar animated striped now={progress[3]}  className='progress-4' label={'Languages'}/>
+            </Col>
+          <Col md={3}></Col>
+
         </Row>
       </Container>
     </>
